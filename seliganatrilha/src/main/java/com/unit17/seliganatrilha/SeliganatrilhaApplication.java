872d45aa06d1f781423a48c0116f1deb2b406333
fernandobituco <fernandoangelima@gmail.com>
2022-09-28 14:39:43 -0300

    Projeto base do spring initializr

---
+++ seliganatrilha/src/main/java/com/unit17/seliganatrilha/SeliganatrilhaApplication.java
@@ -0,0 +1,13 @@
+package com.unit17.seliganatrilha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeliganatrilhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeliganatrilhaApplication.class, args);
+	}
+
+}
---
+++ seliganatrilha/src/test/java/com/unit17/seliganatrilha/SeliganatrilhaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unit17.seliganatrilha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeliganatrilhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
