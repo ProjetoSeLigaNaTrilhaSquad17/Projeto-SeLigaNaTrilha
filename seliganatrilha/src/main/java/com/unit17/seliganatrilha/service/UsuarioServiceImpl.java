1be68a1cf9482e2c5951626e7ad0e0cf328ddb37
fernandobituco <fernandoangelima@gmail.com>
2022-09-30 09:08:41 -0300

    Modificada a camada de serviço para ter uma interface e sua implementação

---
+++ seliganatrilha/src/main/java/com/unit17/seliganatrilha/service/UsuarioServiceImpl.java
@@ -0,0 +1,39 @@
+package com.unit17.seliganatrilha.service;
+
+import com.unit17.seliganatrilha.dtos.UsuarioDto;
+import com.unit17.seliganatrilha.models.Usuario;
+import com.unit17.seliganatrilha.repositories.UsuarioRepository;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+public class UsuarioServiceImpl implements UsuarioService {
+
+    final UsuarioRepository usuarioRepository;
+
+    public UsuarioServiceImpl(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    public List<Usuario> findAll() {
+        return usuarioRepository.findAll();
+    }
+
+    @Transactional
+    public void save(UsuarioDto novoUsuarioDto) {
+        Usuario novoUsuario = new Usuario(
+                novoUsuarioDto.getNome(),
+                novoUsuarioDto.getEmail(),
+                novoUsuarioDto.getCpf(),
+                novoUsuarioDto.getSenha());
+
+        usuarioRepository.save(novoUsuario);
+    }
+
+    @Transactional
+    public void delete(Usuario usuarioDeletar) {
+        usuarioRepository.delete(usuarioDeletar);
+    }
+}
